b92a555ff7fc118546cc4b6be0f70e346ab89bc9
Soumayjit Chakraborty <soumyo.chak1999@gmail.com>
2023-02-14 23:12:30 +0530

    ChatAiFrontend Ready

--- src/Components/Canvas.jsx
+++ src/Components/Canvas.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useRef } from "react";
 import { AiOutlineSend } from "react-icons/ai";
-import { BiBot, BiUserCircle } from "react-icons/bi";
+// import { BiBot, BiUserCircle } from "react-icons/bi";
 import "../Assets/Css/Chathead.css";
 
 export const Canvas = () => {
@@ -9,6 +9,7 @@ export const Canvas = () => {
   let form = null;
   let chatContainer = null;
   let loadInterval = null;
+  const baseURL = "https://chataibackendservice.onrender.com/";
 
   const getformData = () => {
     form = document.querySelector("form");
@@ -26,19 +27,17 @@ export const Canvas = () => {
 
   const typingHandler = (element, text) => {
     let index = 0;
-    setInterval(() => {
+    let interval = setInterval(() => {
       if (index < text.length) {
-        element.innerHTML += text.charAt(index);
+        if (text.charAt(index) === "\n") element.innerHTML += "<br/>";
+        else { element.innerHTML += text.charAt(index); }
         index++;
       } else {
-        clearInterval(loadInterval);
+        clearInterval(interval);
       }
-    }, 30);
+    }, 40);
   };
 
-  const generateMessage = () => {
-    const message = document.createElement("div");
-  };
 
   const generateID = () => {
     const timestamp = Date.now();
@@ -48,18 +47,19 @@ export const Canvas = () => {
   };
 
   const chatDecor = (isAI, value, id) => {
-    return `
-          <div class="wrapper ${isAI && "ai"}">
-          <div class="chat">
+    return `<div class="chathead-container">
           <div class="profile">
-          <img class="" src="${
+          <img class="profile_img" src="${
             isAI
               ? "https://img.icons8.com/fluency/40/null/chatbot.png"
               : "https://img.icons8.com/offices/40/null/user.png"
           }" 
           alt="${isAI ? "ai" : "user"}"/>
           </div>
+          <div class="wrapper ${isAI ? "ai" : "user"}">
+          <div class="chat">
           <div calss="chathead" id="${id}"> ${value}
+          </div>
           </div>
           </div>
           </div>
@@ -70,23 +70,46 @@ export const Canvas = () => {
     event.preventDefault();
     getformData();
     chatContainer.innerHTML += chatDecor(false, data.current.value);
-    form.reset();
     let id = generateID();
-
     chatContainer.innerHTML += chatDecor(true, " ", id);
     chatContainer.scrollTop = chatContainer.scrollHeight;
     const msgDiv = document.getElementById(id);
     loader(msgDiv);
+    const response = await fetch(baseURL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: data.current.value
+      }),
+    });
+    clearInterval(loadInterval);
+    form.reset();
+    msgDiv.innerHTML = "";
+    if (response.ok ) {
+      let returnedData = await response.json();
+      const parsedData = returnedData.bot.trim();
+      typingHandler(msgDiv, parsedData);
+    } else {
+      let err = await response.text();
+      console.error(err)
+      typingHandler(msgDiv,"Hey Ai Is Down right now");
+    }
   };
+
+  
   return (
-    <div className="flex canvas bg-gray-700 max-w-full min-h-screen">
-      <div
-        className="w-fit ml-5 mt-5 md:m-10 absolute text-white"
-        id="chat_container"
-      ></div>
-      <div className="flex flex-end items-center justify-center min-w-full mt-auto">
+    <div className=" flex canvas bg-gray-700 max-w-full h-screen">
+        <div
+          className="w-fit ml-5 mt-5 md:m-10 absolute text-white md:p-2"
+          id="chat_container"
+        ></div>
+    
+      <div className="flex items-center justify-center min-w-full mt-auto">
         <form
-          className="m-2 text-white bg-gray-600 rounded-lg mb-3 p-2 flex flex-row gap-2 bottom-56 justify-center w-full lg:ml-60 lg:mr-60"
+          className=" z-50 text-white bg-gray-600 rounded-lg mb-3 p-2 flex bottom-48 justify-center w-full m-5 lg:ml-60 lg:mr-60"
+          method="post"
           onSubmit={handleSubmit}
         >
           <textarea
